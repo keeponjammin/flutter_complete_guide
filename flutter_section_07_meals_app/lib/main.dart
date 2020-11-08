047b4d8de6aaab8553b85a36d73c09f7d09221f9
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2020-11-08 12:24:38 +0100

    added named routes in navigation.

--- flutter_section_07_meals_app/lib/main.dart
+++ flutter_section_07_meals_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +29,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
