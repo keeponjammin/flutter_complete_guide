e194333a0c424165c015e01ea22b7b214fd046a1
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2020-11-08 13:48:14 +0100

    added meal page and styling. added folder structure.

--- flutter_section_07_meals_app/lib/screens/category_meals_screen.dart
+++ flutter_section_07_meals_app/lib/screens/category_meals_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'dummy_data.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/cateory-meals';
-  // final String categoryId;
-  // final String categoryTitle;
-
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
@@ -23,8 +20,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(
-            categoryMeals[index].title,
+          return MealItem(
+            id: categoryMeals[index].id,
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity: categoryMeals[index].complexity,
           );
         },
         itemCount: categoryMeals.length,
